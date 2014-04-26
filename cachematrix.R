31caa1e1b04110389c8a0d4380986698771ac660
Dragana <d.stevanovic89@gmail.com>
2014-04-26 03:30:14 +0200

    first commit of cachematrix.r

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,43 @@
-## Put comments here that give an overall description of what your
-## functions do
+## First function will be used for implementing a way to calculate inverse 
+## of a matrix, and the second function will be used to return (and calculate 
+## if needed) cached value of that inverse matrix.
 
-## Write a short comment describing this function
+### This function sets and gets value of matrix, and sets and gets inverse of
+### the same matrix.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    c <- NULL
+    set <- function(y) {
+        x <<- y
+        c <<- NULL
+    }
+    get <- function() x
+    inverse<-function(){
+        ...
+    }
+    setinverse <- function(inverse) c <<- inverse ##calculate inverse
+    getinverse <- function() c
+    list(set = set, get = get,
+         setinverse = setinverse,
+         getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+### This function will first check if the inverse of matrix 'x' has already
+### been calculated. If we can get cached data (c is not null), function
+### will return that cached data 'c'. Otherwise, if cashed data is null, this 
+### function will first get matrix, next calculate inverse, after that set 
+### inversed matrix as cach, and finally return cached inverse matrix.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    ## Return a matrix that is the inverse of 'x'
+    c <- x$getinverse()
+    if(!is.null(c)) {
+        message("getting cached data")
+        return(c)                     ### returning cached inverse matrix
+    }
+    data <- x$get()                   ### getting matrix    
+    c <- inverse(data,...)            ### calculating inverse matrix
+    x$setinverse(c)                   ### setting cache of inverse matrix
+    c                                 ### returning cached inverse matrix
 }
